a07ec71c16aada6f6262921126d8a7576e01078d
Bradlee King <82820990+kingofbrad@users.noreply.github.com>
2022-06-21 21:36:04 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <Home />
+    
+    </>
   );
 }
 
