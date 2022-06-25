7a9b22ecced9e2b52b7d1c0f9a19c43c5af88e29
kingofbrad <82820990+kingofbrad@users.noreply.github.com>
2022-06-25 19:42:55 +0100

    Finished the Updated Section and Created the Popular section
    I have finished the Updated Section of the website and then I have created the popular section of the website.
    
    My plan is to add an API to the sections to allow them to automatically update when i change an entry.
    
    My plan is to add the books section for the website and link it to the landing page using React-Router-Dom to connect them.

--- src/components/sub-components/Popularsection.js
+++ src/components/sub-components/Popularsection.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import TriageBookCover from '../Assets/book_covers/book-triage.png'
-import SOTHCover from '../Assets/cover-images/season-of-the-hunted-cover.jpeg'
-import OutBreakPerfected from '../Assets/weapon-art/Outbreak-Perfected.jpg'
-import Cayde6 from '../Assets/npc-art/Cayde-6.PNG'
-import Mithrax from '../Assets/npc-art/Mithrax-cover.avif'
-import DrifterGambit from '../Assets/book_covers/Drifters-Gambit.png'
-import SivaCover from '../Assets/cover-images/Siva-Cover.jpeg'
+import SOTHCover from '../../Assets/cover-images/season-of-the-hunted-cover.jpeg'
+import OutBreakPerfected from '../../Assets/weapon-art/Outbreak-Perfected.jpg'
+import ErisMorn from '../../Assets/npc-art/Eris-Morn-Cover-Art.jpeg'
+import Mithrax from '../../Assets/npc-art/Mithrax-cover.avif'
+import DrifterGambit from '../../Assets/book_covers/Drifters-Gambit.png'
+import SivaCover from '../../Assets/cover-images/Siva-Cover.jpeg'
+import BookTriage from '../../Assets/book_covers/book-triage.png'
+import Tresspasser from '../../Assets/weapon-art/Tresspasser.jpeg'
+import RasputinCover from '../../Assets/cover-images/Rasputin-Cover.png'
 
-
-const Updatedsection = () => {
-    return (
-        <div className='flex justify-center items-center'>
+const Popularsection = () => {
+  return (
+    <div className='flex justify-center items-center pt-[70px]'>
             <div className='w-[400px] sm:w-[700px] md:w-[1200px] mx-auto'>
                 <div>
-                    <h1 className='font-bold text-3xl border-b-2 py-3'>Updates</h1>
+                    <h1 className='font-bold text-3xl border-b-2 py-3'>Popular</h1>
                 </div>
                 <div className='grid grid-11 grid-cols-1  md:grid-cols-3 sm:grid-cols-2 gap-4 pt-3 relative'>
                     {/* First Card */}
@@ -24,7 +25,7 @@ const Updatedsection = () => {
                                 <div className=' pt-[150px] absolute'>
                                     <div className='grid'>
                                         <div className='pl-3' >
-                                            <p className='text-[18px] font-bold  border-b-2 uppercase pb-1 '>Latest Release</p>
+                                            <p className='text-[18px] font-bold  border-b-2 uppercase pb-1 '>Season</p>
                                             <p className='pb-4 text-2xl font-bold'>Season of the Hunted</p>
                                         </div>
                                     </div>
@@ -36,12 +37,12 @@ const Updatedsection = () => {
                     <div className='sm:grid-col-end-2'>
                         <a href='/'>
                             <div className='shadow-lg shadow-[#040c16] group container flex items-center mx-auto content-div '>
-                                <div className='w-full content-div opacity-[0.4] rounded-md' style={{ backgroundImage: `url(${OutBreakPerfected})` }}></div>
+                                <div className='w-full content-div opacity-[0.4] rounded-md' style={{ backgroundImage: `url(${Tresspasser})` }}></div>
                                 <div className='absolute pt-[150px]'>
                                     <div className='grid'>
                                         <div className='pl-3' >
-                                            <p className='text-[18px] font-bold  border-b-2 uppercase pb-1 '>Recent Entry</p>
-                                            <p className='pb-4 text-2xl font-bold'>Outbreak Prime</p>
+                                            <p className='text-[18px] font-bold  border-b-2 uppercase pb-1 '>Weapon</p>
+                                            <p className='pb-4 text-2xl font-bold'>Tresspasser</p>
                                         </div>
                                     </div>
                                 </div>
@@ -51,12 +52,12 @@ const Updatedsection = () => {
                     {/* Third Card */}
                     <a href="">
                         <div className='shadow-lg shadow-[#040c16] group container flex items-center mx-auto content-div'>
-                            <div className='w-full content-div opacity-[0.4] rounded-md' style={{ backgroundImage: `url(${Cayde6})` }}></div>
+                            <div className='w-full content-div opacity-[0.4] rounded-md' style={{ backgroundImage: `url(${ErisMorn})` }}></div>
                             <div className='absolute pt-[150px]'>
                                 <div className='grid'>
                                     <div className='pl-3' >
-                                        <p className='text-[18px] font-bold  border-b-2 uppercase pb-1 '>Recent Category</p>
-                                        <p className='pb-4 text-2xl font-bold'>Cayde-6</p>
+                                        <p className='text-[18px] font-bold  border-b-2 uppercase pb-1 '>Vendor</p>
+                                        <p className='pb-4 text-2xl font-bold'>Eris Morn</p>
                                     </div>
                                 </div>
                             </div>
@@ -69,7 +70,7 @@ const Updatedsection = () => {
                             <div className='absolute pt-[150px]'>
                                 <div className='grid'>
                                     <div className='pl-3' >
-                                        <p className='text-[18px] font-bold  border-b-2 uppercase pb-1 '>Recent Category</p>
+                                        <p className='text-[18px] font-bold  border-b-2 uppercase pb-1 '>Vendor</p>
                                         <p className='pb-4 text-2xl font-bold'>Mithrax</p>
                                     </div>
                                 </div>
@@ -79,12 +80,12 @@ const Updatedsection = () => {
                     {/* Fifth Card */}
                     <a href="">
                         <div className='shadow-lg shadow-[#040c16] group container flex items-center mx-auto content-div'>
-                            <div className='w-full content-div opacity-[0.4] rounded-md' style={{ backgroundImage: `url(${DrifterGambit})` }}></div>
+                            <div className='w-full content-div opacity-[0.4] rounded-md' style={{ backgroundImage: `url(${BookTriage})` }}></div>
                             <div className='absolute pt-[150px]'>
                                 <div className='grid'>
                                     <div className='pl-3' >
-                                        <p className='text-[18px] font-bold  border-b-2 uppercase pb-1 '>Recent Book</p>
-                                        <p className='pb-4 text-2xl font-bold'>Drifter&#39;s Gambit</p>
+                                        <p className='text-[18px] font-bold  border-b-2 uppercase pb-1 '>Book</p>
+                                        <p className='pb-4 text-2xl font-bold'>Season of the Hunted</p>
                                     </div>
                                 </div>
                             </div>
@@ -93,12 +94,12 @@ const Updatedsection = () => {
                     {/* Sixth Card */}
                     <a href="">
                         <div className='shadow-lg shadow-[#040c16] group container flex items-center mx-auto content-div'>
-                            <div className='w-full content-div opacity-[0.4] rounded-md' style={{ backgroundImage: `url(${SivaCover})` }}></div>
+                            <div className='w-full content-div opacity-[0.4] rounded-md' style={{ backgroundImage: `url(${RasputinCover})` }}></div>
                             <div className='absolute pt-[150px]'>
                                 <div className='grid'>
                                     <div className='pl-3' >
-                                        <p className='text-[18px] font-bold  border-b-2 uppercase pb-1 '>Recently Updated</p>
-                                        <p className='pb-4 text-2xl font-bold'>SIVA</p>
+                                        <p className='text-[18px] font-bold  border-b-2 uppercase pb-1 '>Category</p>
+                                        <p className='pb-4 text-2xl font-bold'>Rasputin</p>
                                     </div>
                                 </div>
                             </div>
@@ -109,7 +110,7 @@ const Updatedsection = () => {
                 </div>
             </div>
         </div>
-    )
+  )
 }
 
-export default Updatedsection
+export default Popularsection
--- src/pages/landingpage.js
+++ src/pages/landingpage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Updatedsection from '../components/updatedsection'
+import Popularsection from '../components/sub-components/Popularsection'
 
 const Landingpage = () => {
   return (
     // Main Container
-    <div className="bg-[#19191B] w-full h-full sm:h-screen md:h-screen text-gray-300 pt-[100px]">
+    <div className="bg-[#19191B] w-full h-full sm:h-full md:h-full text-gray-300 pt-[100px]">
       <Updatedsection />
-      
+      <Popularsection />
       
     </div>
   )
