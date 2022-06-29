f703da35f43b2ed71ed33d656e88ffe27bdc9074
kingofbrad <82820990+kingofbrad@users.noreply.github.com>
2022-06-29 18:24:36 +0100

    update

--- src/components/sub-components/Card.js
+++ src/components/sub-components/Card.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import'../sub-components/Card.css'
+import '../sub-components/Card.css'
 import Siva from '../../Assets/cover-images/Siva-Cover.jpeg'
 import CardRecords from '../../Json/cards.json'
 
@@ -9,22 +9,17 @@ import CardRecords from '../../Json/cards.json'
 const Card = () => {
     return (
         <div style={{ backgroundImage: `url(${Siva})` }}
-        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
-        {/* Hover Effects */}
-        <div className='opacity-100 '>
-            <span className='text-2xl font-bold text-white tracking-wider'>
-                Cluttered Charactors
-            </span>
-            <div className='pt-8 text-center'>
-                <span>Recently Updated</span>
-                <span></span>
+            {/* Hover Effects */}
+            <div className='text-center flex text-2xl text-white'>
+                {CardRecords && CardRecords[0].title}
             </div>
-        </div>
+            <div>
+                {CardRecords && CardRecords[1].content}
+            </div>
 
-        { CardRecords && CardRecords[0].title }
-
-        {/* {
+            {/* {
             CardRecords && CardRecords.map( record => {
                 return (
                     <div className='' key={ record.id}>
@@ -42,7 +37,7 @@ const Card = () => {
         } */}
 
 
-    </div>
+        </div>
     )
 }
 
